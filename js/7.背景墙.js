9c6bf0f0bc6c90ac67d83681bf7c9b577ceaa5ec
unknown <309425239@qq.com>
2017-09-11 17:12:46 +0800

    整合html文件夹

--- js/7.背景墙.js
+++ js/7.背景墙.js
@@ -1,8 +1,8 @@
 var imgs = document.getElementsByTagName('li');
 for(var i=0;i<imgs.length;i++){
 	imgs[i].index = i;
-	imgs[i].style.backgroundImage = "url(img/img" + (imgs[i].index +1) + ".jpg)";
+	imgs[i].style.backgroundImage = "url(../img/img" + (imgs[i].index +1) + ".jpg)";
 	imgs[i].onclick = function(){
-		document.body.style.backgroundImage = "url(img/img" + (this.index +1) + ".jpg)";
+		document.body.style.backgroundImage = "url(../img/img" + (this.index +1) + ".jpg)";
 	}
 }
