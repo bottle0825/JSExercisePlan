c794fa582b6e60ed6543821cd372f8d7ce0ea90e
unknown <309425239@qq.com>
2017-09-11 21:05:33 +0800

    8.背景墙优化

--- js/7.背景墙.js
+++ js/7.背景墙.js
@@ -1,4 +1,5 @@
 var imgs = document.getElementsByTagName('li');
+document.body.style.backgroundImage = "url(../img/img1.jpg)";
 for(var i=0;i<imgs.length;i++){
 	imgs[i].index = i;
 	imgs[i].style.backgroundImage = "url(../img/img" + (imgs[i].index +1) + ".jpg)";
