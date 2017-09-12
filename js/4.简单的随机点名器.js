a669c797f78d22a3b0a54e91290cb9af21f02ee3
unknown <309425239@qq.com>
2017-09-12 10:19:18 +0800

    4.随机点名器优化

--- js/4.简单的随机点名器.js
+++ js/4.简单的随机点名器.js
@@ -1,6 +1,7 @@
 var names = ['宋洁钰','吴有文','杨艳','陈璐','杨彩晗','倪一','潘倩倩','卢丹','韩文','倪君','袁奔','龚琪','龚洁','袁科薇','丁悦',
 '党方超','洪政','黄继安','陈渊博','王冬','曹栋','范成磊','陈昊','魏新波','葛翰林','黄达','钱唐','孙袁文'];
 var flag = -1;
+var ran = null;
 function Choose(){
 	flag = 0-flag;
 	var show = document.getElementById('show');
@@ -9,8 +10,7 @@ function Choose(){
 	btn.innerHTML = '停止';
 	if(flag==1){
 		i = setInterval(function(){
-			ran = Math.random()*names.length;
-			ran = parseInt(ran,10);
+			ran = Math.floor(Math.random()*names.length);
 			show.innerHTML = names[ran];
 		},100);
 	}else{
