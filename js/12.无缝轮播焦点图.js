fa7d1a307343cbf341232809271660fb639fba2b
unknown <309425239@qq.com>
2017-09-16 19:21:40 +0800

    12.无缝滚动焦点图（无缝滚动实现）

--- js/12.无缝轮播焦点图.js
+++ js/12.无缝轮播焦点图.js
@@ -1,47 +1,94 @@
 var show = document.getElementById('show');
+var left = document.getElementById('left');
+var right = document.getElementById('right');
 var ul = show.children[0];
-var num = 0;
-var nums = 0;
+var lis = ul.children;
+var numli = 0;//---初始标签的位置
+var num = -800;//---初始图片的位置
+var numche = 0;//---初始计时点
 var timer = null;
-var timers = null;
+var timerche = null;
 
-timer = setInterval(autoPlay,20);
 
+//---定时器控制图片轮播间隔和时间
+timerche = setInterval(autoPlay,4000);
+
+//---实现滚动与停止
 function autoPlay(){
-	num-=10;
-	if(num == -4000){
-		num=0;
-		timers = setInterval(function(){
-			if(nums<5){
-				nums++;
-				console.log(nums);
-			}
-		},1000);
-		if(nums>5){
-			clearInterval(timers);
-			nums=0;
-			timer = setInterval(autoPlay,20);
+	clearInterval(timer);
+	timer = setInterval(function(){
+		num-=10;
+		numche++;
+		ul.style.left = num+'px';
+		if(num== -4800){
+			num = -800;
 		}
-	}else if(num == 0||num == -800||num == -1600||num == -2400||num == -3200){
-		clearInterval(timer);
-		timers = setInterval(function(){
-			if(nums<5){
-				nums++;
-				console.log(nums);
-			}
-		},1000);
-		if(nums>5){
-			clearInterval(timers);
-			nums=0;
-			timer = setInterval(autoPlay,20);
+		if(numche == 80){
+			numche = 0;
+			clearInterval(timer);
 		}
+	},20);
+	Check();
+}
+
+//---焦点方法
+for(let i=0;i<5;i++){
+	lis[i].onmouseover = function(){
+		clearInterval(timerche);
+		var numche = -(i+1)*800;//---焦点的位置
+		timer = setInterval(function(){
+			num-=10;
+			numche++;
+			ul.style.left = num+'px';
+			if(num== -4800){
+				num = -800;
+			}
+			if(numche == 80){
+				numche = 0;
+				clearInterval(timer);
+			}
+		},20);
 	}
-	ul.style.left = num+'px';
+}
+
+function Check(){
+	if((-num)%800 == 0){
+		numli = ((-num)/800)-1;
+		for(var i=0;i<5;i++){
+			lis[i].className = '';
+		}
+		lis[numli].className = 'sel';
+		console.log(lis[numli]);
+	}
 }
 
 show.onmouseover = function(){
-	clearInterval(timer);
+	clearInterval(timerche);
 }
 show.onmouseout = function(){
-	timer = setInterval(autoPlay,20);
+	timerche = setInterval(autoPlay,4000);
 }
+
+//---点击上一张
+left.onclick =  function(){
+	clearInterval(timer);
+	timer = setInterval(function(){
+		num+=10;
+		numche++;
+		ul.style.left = num+'px';
+		if(num == -800){
+			num = -4800;
+		}
+		if(numche == 80){
+			numche = 0;
+			clearInterval(timer);
+		}
+	},20);
+	Check();
+}
+
+//---点击下一张
+right.onclick =  function(){
+	autoPlay();
+}
+
